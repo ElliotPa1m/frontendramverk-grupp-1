786cbb8e55f6a6fc0a58505243e193dcb6386962
Robert <r.eriksson86@gmail.com>
2026-05-15 18:51:15 +0200

    Added getRecipeByid, added better comments (JSDoc)

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,29 @@
 import apiClient from './axiosConfig';
 
+
+/*----------------------------------------------/ 
+/                                               /
+/       API for GET recipes using Axios         /     
+/                                               /
+/----------------------------------------------*/
+
+
 /**
- * Fetches recipes from the API with optional filtering.
+ * Fetches recipes from the API with optional filtering. Full list of possible params here: https://recipeapi.io/docs/resources/recipes/
+ * Suggested params below:
  *
- * @param {Object} filters - Optional filter parameters
+ * @param {Object} [filters] - Optional filter parameters
  * @param {string} [filters.search] - Search query string
  * @param {string} [filters.cuisine] - Cuisine type (e.g. 'italian', 'asian')
- * @param {string} [filters.dietary_tags] - Dietary tags (e.g. 'vegan', 'gluten-free')
+ * @param {string} [filters.dietary_tags] - Dietary tags (e.g. 'vegan', 'gluten-free') Currently only accepts a single value
  * @param {string} [filters.meal_type] - Meal type (e.g. 'breakfast', 'dinner')
- * @param {number} [filters.prep_time_max] - Maximum prep time in minutes
+ * @returns {Promise<Object>} Recipe list and metadata
+ * @example
+ * getAllRecipes({ cuisine: 'italian', meal_type: 'starter' });
  */
 export const getAllRecipes = async (filters = {}) => {
   try {
-    const response = await apiClient.get('recipes', {
+    const response = await apiClient.get('/recipes', {
       params: {
         lang: 'en',
         per_page: 10,
@@ -22,6 +33,23 @@ export const getAllRecipes = async (filters = {}) => {
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch recipes ' + error.message, {
+      cause: error,
+    });
+  }
+};
+
+/**
+ * Fetches recipe by id.
+ *
+ * @param {string} id*
+ * @returns {Promise<Object>} Single recipe object
+ */
+export const getRecipeById = async id => {
+  try {
+    const response = await apiClient.get(`/recipes/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch recipe ' + id + ': ' + error.message, {
       cause: error,
     });
   }
