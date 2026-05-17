332ace75e74f78fd15277e7f5e768c6d1972a4db
Carolina <carolina.warntorp@outlook.com>
2026-05-17 23:35:14 +0200

    build first draft image  layout

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,3 +1,9 @@
-export const Image = () => {
-  return <>image</>;
+export const Image = ({ imgUrl, recipeName }) => {
+  return (
+    <img
+      className="w-full object-cover object-center"
+      src={imgUrl}
+      alt={`image of ${recipeName}`}
+    />
+  );
 };
