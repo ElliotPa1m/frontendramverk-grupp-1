a29d55635f216cc103f4c4bb2d05492fc738d0e2
Carolina <carolina.warntorp@outlook.com>
2026-05-17 23:35:28 +0200

    build first draft rating layout

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,20 @@
-export const Rating = () => {
+export const Rating = ({ rating }) => {
+  const generateStars = (r) => {
+    const rounded = Math.round(r * 2) / 2;
+
+    const fullStars = Math.floor(rounded);
+    const halfStar = rounded % 1 !== 0;
+    const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+
+    return (
+      "★".repeat(fullStars) + (halfStar ? "½" : "") + "☆".repeat(emptyStars)
+    );
+  };
   return (
     <>
-      <div>rating</div>
+      <div className="flex items-center">
+        {generateStars(rating)} <span className="text-xs">({rating})</span>
+      </div>
     </>
   );
 };
