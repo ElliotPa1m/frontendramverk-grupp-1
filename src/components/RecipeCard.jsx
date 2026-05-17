fbe3187031a96a6b9d1224724b35f521e2c22d06
Carolina <carolina.warntorp@outlook.com>
2026-05-17 23:36:19 +0200

    build first draft recipe card layout

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,14 +1,32 @@
-export const RecipeCard = () => {
+import { Image } from "./Image";
+import { IconButton } from "./IconButton";
+import { IconWithInfo } from "./IconWithInfo";
+import { RecipeCardInfoSection } from "./RecipeCardInfoSection";
+import { useState } from "react";
+
+export const RecipeCard = ({ recipe, rating }) => {
+  const [saved, setSaved] = useState(false);
+
   return (
-    <div className="border">
-      RecipeCard
-      <p>img</p>
-      IconButton IconWithInfo
-      <div>
-        info section
-        <div>rating</div>
-        <span>readmore</span>
+    <div className="border rounded-md overflow-hidden min-w-fit max-w-70 bg-amber-50">
+      <div className="relative">
+        <Image imgUrl={recipe.image} recipeName={recipe.name} />
+        <div className="absolute top-2 right-3">
+          <IconButton
+            icon={saved ? "saved" : "toSave"}
+            actionHandler={() => setSaved(!saved)}
+          />
+        </div>
+
+        <div className="absolute bottom-2 left-3">
+          <IconWithInfo icon={"time"} timeToCook={recipe.meta.total_time} />
+        </div>
       </div>
+      <RecipeCardInfoSection
+        recipeName={recipe.name}
+        dietary={recipe.dietary}
+        rating={rating}
+      />
     </div>
   );
 };
