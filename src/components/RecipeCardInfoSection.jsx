0033371c9301c69fd96974592b6877ed67bf2cd5
Carolina <carolina.warntorp@outlook.com>
2026-05-17 23:35:59 +0200

    build first draft info section layout

--- src/components/RecipeCardInfoSection.jsx
+++ src/components/RecipeCardInfoSection.jsx
@@ -1,13 +1,13 @@
 import { Rating } from "./Rating";
 
-export const RecipeCardInfoSection = () => {
+export const RecipeCardInfoSection = ({ recipeName, dietary, rating }) => {
   return (
-    <div>
-      <h3>meal info</h3>
-      <h2>Recipe name</h2>
-      <div>
-        <Rating />
-        <span>readmore</span>
+    <div className="mx-4 mt-4 mb-2 flex flex-col gap-1">
+      <h3 className="text-sm">{dietary.flags.map((d) => d).join(" • ")}</h3>
+      <h2 className="text-xl">{recipeName}</h2>
+      <div className="flex justify-between">
+        <Rating rating={rating} />
+        <span className="text-sm">read more →</span>
       </div>
     </div>
   );
