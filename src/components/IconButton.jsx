2ef67c22af5bfc4d8b521c606e4f979491a8849f
Carolina <carolina.warntorp@outlook.com>
2026-05-17 23:34:34 +0200

    build first draft icon btn layout

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,7 +1,23 @@
-export const IconButton = () => {
+export const IconButton = ({ icon, actionHandler }) => {
+  let dataToShow = (i) => {
+    switch (i) {
+      case "toSave":
+        return `toSave`;
+      case "saved":
+        return `saved`;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <button>icon</button>
+      <button
+        className="hover:cursor-pointer rounded-xl bg-white/80 px-2"
+        onClick={actionHandler}
+      >
+        {dataToShow(icon)}
+      </button>
     </>
   );
 };
