f8d92da3f1dee575b63d12dc6b24c19c9b3ab896
Carolina <carolina.warntorp@outlook.com>
2026-05-18 14:50:17 +0200

    bug class

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,13 +2,13 @@ export const Icon = ({ icon }) => {
   let iconToShow = (i) => {
     switch (i) {
       case "favorite":
-        return <i class="fa-solid fa-heart"></i>;
+        return <i className="fa-solid fa-heart"></i>;
       case "notFavorite":
-        return <i class="fa-regular fa-heart"></i>;
+        return <i className="fa-regular fa-heart"></i>;
       case "time":
-        return <i class="fa-regular fa-clock"></i>;
+        return <i className="fa-regular fa-clock"></i>;
       default:
-        return <i class="fa-regular fa-circle"></i>;
+        return <i className="fa-regular fa-circle"></i>;
     }
   };
   return <>{iconToShow(icon)}</>;
