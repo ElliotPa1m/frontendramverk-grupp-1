5f6a65e1f1c46c3394d74bfffdc2bfe08a63e636
Carolina <carolina.warntorp@outlook.com>
2026-05-18 20:27:56 +0200

    ref add rating function after change of api

--- src/components/RecipeCardList.jsx
+++ src/components/RecipeCardList.jsx
@@ -1,12 +1,17 @@
 import { RecipeCard } from "./RecipeCard";
 
 export const RecipeCardList = ({ arr }) => {
+  // eslint-disable-next-line react-hooks/purity
+  const createRating = () => (Math.random() * 5).toFixed(1);
   return (
     <div className="mt-4 flex flex-wrap gap-4 justify-center">
       {arr.map((r) => {
         return (
-          <div key={r.recipe.id}>
-            <RecipeCard recipe={r.recipe} rating={r.rating} />
+          <div key={r.idMeal}>
+            <RecipeCard
+              recipe={r}
+              rating={r.rating ? r.rating : createRating()}
+            />
           </div>
         );
       })}
