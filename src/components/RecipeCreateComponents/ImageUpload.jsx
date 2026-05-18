1433d7488f8547bdda61d90d5618e858cf498129
StevenCesario <steven.cesario@proton.me>
2026-05-18 16:37:47 +0200

    init commit

---
+++ src/components/RecipeCreateComponents/ImageUpload.jsx
@@ -0,0 +1,3 @@
+// To be coded
+// Will handle the local file selection, basic security filtering (size/type), 
+// and show an instant preview using URL.createObjectURL so that the user knows it worked. Instant feedback for good UX
