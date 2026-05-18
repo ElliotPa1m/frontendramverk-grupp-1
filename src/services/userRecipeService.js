172491c89f8f3f5e4e79fa48fa7ed890b931d605
Robert <r.eriksson86@gmail.com>
2026-05-18 23:36:40 +0200

    WIP, need to find a safe way to generate Ids to user created recipes.

---
+++ src/services/userRecipeService.js
@@ -0,0 +1,51 @@
+/*----------------------------------------------/ 
+/                                               /
+/               userRecipeService               /     
+/                                               /
+/----------------------------------------------*/
+
+//getUserRecipes()
+//getUserRecipeById(id)
+//saveUserRecipe(recipe)
+//updateUserRecipe(recipe)
+//deleteUserRecipe(id)
+
+const STORAGE_KEY = 'userRecipes';
+
+export const getUserRecipes = () => {
+  const storedRecipes = localStorage.getItem(STORAGE_KEY);
+  return storedRecipes ? JSON.parse(storedRecipes) : [];
+};
+
+export const getUserRecipeById = id => {
+  return getUserRecipes().find(recipe => recipe.id == id);
+};
+
+// TEMP COMMENT - needs a reliable way to generate Ids för new user created recipes.
+export const saveUserRecipe = recipe => {
+  const storedRecipes = getUserRecipes();
+  const newRecipe = {
+    ...recipe,
+    id: 'need id-generation',
+  };
+
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify([...storedRecipes, newRecipe]),
+  );
+};
+
+export const updateUserRecipe = updatedRecipe => {
+  const storedRecipes = getUserRecipes();
+  const updatedRecipes = storedRecipes.map(recipe =>
+    recipe.id == updatedRecipe.id ? updatedRecipe : recipe,
+  );
+
+  (localStorage.setItem(STORAGE_KEY), JSON.stringify(updatedRecipes));
+};
+
+export const deleteUserRecipe = id => {
+  const storedRecipes = getUserRecipes();
+  const filteredRecipes = storedRecipes.filter(recipe => recipe.id !== id);
+  (localStorage.setItem(STORAGE_KEY), JSON.stringify(filteredRecipes));
+};
