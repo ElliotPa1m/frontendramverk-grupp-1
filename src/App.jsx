cd0f3ae8389bb6a74437cc11af879e5164bdf255
Carolina <carolina.warntorp@outlook.com>
2026-05-18 20:33:06 +0200

    ref saved page to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import { CreatedFavoritePage } from "./pages/CreatedFavoritePage";
 // import RecipeDetailsPage from './pages/RecipeDetailsPage';
 
 function App() {
@@ -11,11 +12,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* The Layout wrapper is a wrapper around all of our page routes */}
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           {/* All of these are commented out until the pages are created and ready to be inserted as the element */}
           {/* <Route index element={} /> */}
           {/* <Route path='search' element={} /> */}
-          {/* <Route path='saved' element={} /> */}
+          <Route path="saved" element={<CreatedFavoritePage />} />
           {/* <Route path='create' element={} /> */}
           {/* <Route path='recipe/:id' element={<RecipeDetailsPage />} /> */}
         </Route>
