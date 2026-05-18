e6e821ce2745437bf4202f074fb1270dbf387fe7
Carolina <carolina.warntorp@outlook.com>
2026-05-18 20:28:47 +0200

    ref change structure after api change

--- src/components/RecipeCardInfoSection.jsx
+++ src/components/RecipeCardInfoSection.jsx
@@ -1,9 +1,11 @@
 import { Rating } from "./Rating";
 
-export const RecipeCardInfoSection = ({ recipeName, dietary, rating }) => {
+export const RecipeCardInfoSection = ({ recipeName, info, rating }) => {
+  console.log(recipeName, info, rating);
+
   return (
     <div className="mx-4 mt-4 mb-2 flex flex-col gap-1">
-      <h3 className="text-sm">{dietary.flags.map((d) => d).join(" • ")}</h3>
+      <h3 className="text-sm">{info}</h3>
       <h2 className="text-xl">{recipeName}</h2>
       <div className="flex justify-between">
         <Rating rating={rating} />
