1da83ed3b19e8799f0341d9ee48ff0d3e9d8d913
StevenCesario <steven.cesario@proton.me>
2026-05-19 09:31:18 +0200

    valid types and max size implemented

--- src/components/RecipeCreateComponents/ImageUpload.jsx
+++ src/components/RecipeCreateComponents/ImageUpload.jsx
@@ -1,3 +1,31 @@
-// To be coded
 // Will handle the local file selection, basic security filtering (size/type), 
 // and show an instant preview using URL.createObjectURL so that the user knows it worked. Instant feedback for good UX
+
+import { useState, useEffect } from 'react';
+
+const ImageUpload = ({ onFileSelect, error }) => {
+  const [previewURL, setPreviewURL] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    // Frontend Security: Validate the file type. Current config: jpeg, png and webp allowed
+    const validTypes = ['image/jpeg', 'image/png', 'image/webp'];
+    if (!validTypes.includes(file.type)) {
+      alert('Please upload a JPEG, PNG, or WebP image.');
+      return;
+    }
+
+    // Frontend Security: Validate Size (Max 3MB)
+    if (file.size > 3 * 1024 * 1024) {
+      alert('Image must be under 3MB.');
+      return;
+    }
+  }
+  return (
+    <div>ImageUpload</div>
+  )
+}
+
+export default ImageUpload
