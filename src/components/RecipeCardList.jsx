758c1b2cfe250cb01d17c14a8319d61ec9fee10d
Carolina <carolina.warntorp@outlook.com>
2026-05-19 10:06:36 +0200

    ref add dynamic styles

--- src/components/RecipeCardList.jsx
+++ src/components/RecipeCardList.jsx
@@ -4,10 +4,14 @@ export const RecipeCardList = ({ arr }) => {
   // eslint-disable-next-line react-hooks/purity
   const createRating = () => (Math.random() * 5).toFixed(1);
   return (
-    <div className="mt-4 flex flex-wrap gap-4">
+    <div
+      className="mt-4 mx-auto
+                flex flex-wrap gap-3 items-stretch justify-center
+                w-full max-w-[1250px]"
+    >
       {arr.map((r) => {
         return (
-          <div key={r.idMeal}>
+          <div key={r.idMeal} className="flex basis-[250px] grow max-w-[400px]">
             <RecipeCard
               recipe={r}
               rating={r.rating ? r.rating : createRating()}
