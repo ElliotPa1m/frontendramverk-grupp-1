f7077bfb23be7bf1d32c5b0cddf44f451e81aa7d
Carolina <carolina.warntorp@outlook.com>
2026-05-19 13:20:59 +0200

    build first draft of created recipe page

---
+++ src/pages/AllCreatedRecipePage.jsx
@@ -0,0 +1,16 @@
+import { RecipeCardList } from "../components/RecipeCardList";
+
+export const AllCreatedRecipePage = () => {
+  const recipeArr = [];
+
+  return (
+    <div className="mx-4 my-4">
+      <h2 className="barlow-condensed-regular text-2xl text-text">Favorites</h2>
+      {recipeArr.length !== 0 ? (
+        <RecipeCardList arr={recipeArr} />
+      ) : (
+        <p>You have not created any recipes yet</p>
+      )}
+    </div>
+  );
+};
