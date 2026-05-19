e481728ea21c05add73db771aa9440473729bbfd
Carolina <carolina.warntorp@outlook.com>
2026-05-19 14:11:27 +0200

    ref info structure

--- src/components/RecipeCardInfoSection.jsx
+++ src/components/RecipeCardInfoSection.jsx
@@ -15,9 +15,8 @@ export const RecipeCardInfoSection = ({
     <div className="mx-4 mt-4 mb-2 flex flex-1 flex-col gap-1">
       <h3 className="barlow-condensed-regular text-sm">
         {country} | {cat}
-        {tagArr && <br />}
-        {tagArr && tagArr}
       </h3>
+      {tagArr && <h3 className="barlow-condensed-regular text-sm">{tagArr}</h3>}
       <h2 className="barlow-condensed-regular text-xl">{recipeName}</h2>
       <div className="mt-auto flex justify-between">
         <Rating rating={rating} />
