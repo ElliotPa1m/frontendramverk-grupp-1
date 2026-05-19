ad512fbe4f4bc6c060caa9b4f9be675031ce9da1
Carolina <carolina.warntorp@outlook.com>
2026-05-19 13:25:08 +0200

    ref change header

--- src/pages/AllCreatedRecipePage.jsx
+++ src/pages/AllCreatedRecipePage.jsx
@@ -5,7 +5,9 @@ export const AllCreatedRecipePage = () => {
 
   return (
     <div className="mx-4 my-4">
-      <h2 className="barlow-condensed-regular text-2xl text-text">Favorites</h2>
+      <h2 className="barlow-condensed-regular text-2xl text-text">
+        My Created Recipes
+      </h2>
       {recipeArr.length !== 0 ? (
         <RecipeCardList arr={recipeArr} />
       ) : (
