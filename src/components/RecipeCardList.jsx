a6132c75f03387a6ebaba6942640e07eeb27670f
Carolina <carolina.warntorp@outlook.com>
2026-05-20 10:55:42 +0200

    ref remove conditional

--- src/components/RecipeCardList.jsx
+++ src/components/RecipeCardList.jsx
@@ -1,8 +1,6 @@
 import { RecipeCard } from "./RecipeCard";
 
 export const RecipeCardList = ({ arr }) => {
-  // eslint-disable-next-line react-hooks/purity
-  const createRating = () => (Math.random() * 5).toFixed(1);
   return (
     <div
       className="mt-4 mx-auto
@@ -12,10 +10,7 @@ export const RecipeCardList = ({ arr }) => {
       {arr.map((r) => {
         return (
           <div key={r.idMeal} className="flex basis-[250px] grow max-w-[400px]">
-            <RecipeCard
-              recipe={r}
-              rating={r.rating ? r.rating : createRating()}
-            />
+            <RecipeCard recipe={r} rating={r.rating} />
           </div>
         );
       })}
