eb2683f4fcee1f9157911fd753ec409cb104261c
Robert <r.eriksson86@gmail.com>
2026-05-20 23:58:30 +0200

    added functions isDisabled and handleChange to SearchForm. see comments for their tandem usage.

--- src/components/RecipeSearchComponents/SearchForm.jsx
+++ src/components/RecipeSearchComponents/SearchForm.jsx
@@ -4,7 +4,7 @@ import mainIngredients from '../../data/mainIngredients.json';
 
 //TEMP COMMENT:  Filter bar with a name search input and three dropdown filters. since only one filter can be used/active at a time the others will get disabled automatically.
 
-const SearchForm = ({ onSearch} ) => {
+const SearchForm = ({ onSearch }) => {
   const [search, setSearch] = useState({ filter: null, value: '' });
   const [filterOptions, setFilterOptions] = useState({
     categories: [],
@@ -39,11 +39,23 @@ const SearchForm = ({ onSearch} ) => {
   }, []);
 
   //HandleSearch - meant for submit button
-
   const handleSearch = (e) => {
     e.preventDefault();
     if (!search.filter) return;
     onSearch(search);
+  };
+
+  // Because the mealDB API only allows use of one filter a search this function will make sure that upon use of a filter input the other inputs gets disabled. This was the most 'elegant' solution I could come up with to suit or new (annoying) api.
+  const isDisabled = (filterType) =>
+    search.filter !== null && search.filter !== filterType;
+
+  // for updating the search state (which are the params for getAllRecipes). Runs every time user types in the search field(name) or picks something from a filter dropdown. If a user deletes their search term/filter option, the filter in search useState becomes null, which enables all the filter form options again.
+
+  const handleChange = (filterType, newValue) => {
+    setSearch({
+      filter: newValue ? filterType : null,
+      value: newValue,
+    });
   };
 
   return <div>SearchForm</div>;
