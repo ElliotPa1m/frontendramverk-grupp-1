0775a9e966022fe29b631f8913815c5f436aa13e
Carolina <carolina.warntorp@outlook.com>
2026-05-21 10:51:02 +0200

    ref feature cards to responsive layout

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom";
 
 const FeatureCard = ({ title, description, to, linkText }) => {
-    return (
-        <div className="flex flex-col items-center gap-4 p-8 bg-white rounded-lg shadow-md text-center transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
-            <h2 className="barlow-condensed-regular text-xl text-text">{title}</h2>
-            <p className="barlow-condensed-light text-text">{description}</p>
-            <Link to={to} className="barlow-condensed-light bg-button text-white px-6 py-2 rounded-full">
-                {linkText} →
-            </Link>
-        </div>
-    );
+  return (
+    <div
+      className="flex flex-col items-center gap-2 
+    p-2 pb-4 sm:p-8 
+    h-fit
+    basis-w-[300px] grow max-w-[400px]
+    bg-white shadow-md 
+    rounded-lg
+    text-center 
+    transition-all duration-300 
+    hover:-translate-y-1 hover:shadow-lg"
+    >
+      <h2 className="barlow-condensed-regular text-xl text-text">{title}</h2>
+      <p className="barlow-condensed-light text-text">{description}</p>
+      <Link
+        to={to}
+        className="barlow-condensed-light bg-button text-white px-6 py-2 rounded-full"
+      >
+        {linkText} →
+      </Link>
+    </div>
+  );
 };
 
 export default FeatureCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,72 +1,69 @@
-import { useState, useEffect } from 'react'
-import FeatureCard from '../components/FeatureCard';
-import { RecipeCardList } from '../components/RecipeCardList';
-import { getRandomRecipes } from '../services/api';
+import { useState, useEffect } from "react";
+import FeatureCard from "../components/FeatureCard";
+import { RecipeCardList } from "../components/RecipeCardList";
+import { getRandomRecipes } from "../services/api";
 
 const HomePage = () => {
+  const [randomRecipes, setRandomRecipes] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const [randomRecipes, setRandomRecipes] = useState([])
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchRandomRecipes = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await getRandomRecipes(3);
+        setRandomRecipes(data);
+      } catch (error) {
+        console.log(error.message);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRandomRecipes();
+  }, []);
 
-    useEffect(() => {
-        const fetchRandomRecipes = async () => {
-            try {
-                setLoading(true);
-                setError(null);
-                const data = await getRandomRecipes(3)
-                setRandomRecipes(data)
-            } catch (error) {
-                console.log(error.message)
-                setError(error.message)
-            } finally {
-                setLoading(false);
-            }
-        }
-        fetchRandomRecipes();
-    }, [])
+  if (loading) {
+    return <div className="loading-recipe">Loading recipes...</div>;
+  }
 
-    if (loading) {
-        return <div className="loading-recipe">Loading recipes...</div>;
-    }
-
-    if (error) {
-        return (
-            <div className="error-loading-recipes">
-                <h2>Error loading recipes</h2>
-                <p>{error}</p>
-            </div>
-        );
-    }
-
+  if (error) {
     return (
-        <div>
+      <div className="error-loading-recipes">
+        <h2>Error loading recipes</h2>
+        <p>{error}</p>
+      </div>
+    );
+  }
 
-            <RecipeCardList arr={randomRecipes} />
+  return (
+    <div>
+      <RecipeCardList arr={randomRecipes} />
 
+      <div className="features flex flex-col sm:flex-row sm:flex-wrap gap-8 justify-center p-8">
+        <FeatureCard
+          title="Search for a recipe"
+          description="Searching for a recipe from TheMealDB database"
+          to="/search"
+          linkText="Search here"
+        />
+        <FeatureCard
+          title="Favorite recipes"
+          description="See your favorite recipes"
+          to="/my-recipes"
+          linkText="Favorite recipes"
+        />
+        <FeatureCard
+          title="Upload your own recipe"
+          description="Upload your own recipes here"
+          to="/create"
+          linkText="Upload recipes"
+        />
+      </div>
+    </div>
+  );
+};
 
-            <div className="features flex gap-8 justify-center p-8">
-                <FeatureCard
-                    title="Search for a recipe"
-                    description="Searching for a recipe from TheMealDB database"
-                    to="/search"
-                    linkText="Search here"
-                />
-                <FeatureCard
-                    title="Favorite recipes"
-                    description="See your favorite recipes"
-                    to="/my-recipes"
-                    linkText="Favorite recipes"
-                />
-                <FeatureCard
-                    title="Upload your own recipe"
-                    description="Upload your own recipes here"
-                    to="/create"
-                    linkText="Upload recipes"
-                />
-            </div>
-        </div>
-    )
-}
-
-export default HomePage
+export default HomePage;
