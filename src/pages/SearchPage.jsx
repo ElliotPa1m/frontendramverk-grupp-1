c24930d8d743b07326a12d11ea720e0ce7a8705e
Robert <r.eriksson86@gmail.com>
2026-05-21 16:53:11 +0200

    structuring searchPage: added a useEffect to load random recipes on mount

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,21 +1,34 @@
 import { useState, useEffect } from 'react';
 import SearchFilter from '../components/RecipeSearchComponents/SearchForm';
+import { getCachedRecipes, getRandomRecipes } from '../services/recipeService';
 
 const SearchPage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  // load 10 random recipes on mount
+  useEffect(() => {
+    const loadInitialRecipes = async () => {
+      try {
+        const data = await getRandomRecipes(10);
+        setRecipes(data);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    loadInitialRecipes();
+  }, []);
 
-
-    const handleSearch = () => {
-        console.log('testing')
-    }
-
-
+  
+  const handleSearch = () => {
+    console.log('testing');
+  };
 
   return (
     <div>
       <SearchFilter onSearch={handleSearch} />
     </div>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
