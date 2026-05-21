2cce8fd7bbdf58088c68054eb73aaa5862f5a051
StevenCesario <steven.cesario@proton.me>
2026-05-21 15:34:57 +0200

    SelectInput component select tag

--- src/components/RecipeCreateComponents/SelectInput.jsx
+++ src/components/RecipeCreateComponents/SelectInput.jsx
@@ -3,6 +3,25 @@ const SelectInput = ({ label, options, register, error, placeholder }) => {
     <div>
       <label className="font-semibold text-gray-700">{label}</label>
 
+      <select
+        {...register}
+        className={`border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors bg-white ${
+          error ? 'border-red-500' : 'border-gray-300'
+        }`}
+      >
+        {/* The disabled placeholder option */}
+        <option value="" disabled>
+          {placeholder}
+        </option>
+
+        {/* Dynamically render the list of options (categories or areas in our case) */}
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+
       {/* Hard validation error from Zod */}
       {error && <p>{error}</p>}
     </div>
