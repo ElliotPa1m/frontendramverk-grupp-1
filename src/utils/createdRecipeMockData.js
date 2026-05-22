65adcc9a7aa51b67ee8cf0c82982ee2c9fe24575
Carolina <carolina.warntorp@outlook.com>
2026-05-22 09:41:07 +0200

    ref upd mockdata

--- src/utils/createdRecipeMockData.js
+++ src/utils/createdRecipeMockData.js
@@ -1,5 +1,26 @@
 export const createdMockRecipeArr = [
   {
+    title: "papasta",
+    category: "Pasta",
+    area: "Aruban",
+    tags: ["onepot", "dinner", "fast"],
+    instructions: "boil water\nadd salt\nadd pasta\nfry chicken",
+    ingredients: [
+      {
+        name: "pasta",
+        measure: "100g",
+      },
+      {
+        name: "chicken",
+        measure: "200g",
+      },
+    ],
+    imageUrl:
+      "https://res.cloudinary.com/dxef2u52q/image/upload/v1779435450/owmy4wa28j0zftvigxdk.png",
+    createdAt: "2026-05-22T07:37:31.397Z",
+    id: "7d47ea9b-dc74-42e1-8eaf-646efaeb591b",
+  },
+  {
     title: "Steven's pancakes",
     instructions: "Step 1: pancake\nStep 2: done :)",
     ingredients: [
@@ -46,17 +67,5 @@ export const createdMockRecipeArr = [
       "https://res.cloudinary.com/dxef2u52q/image/upload/v1779363667/pltusiwhatyvnjrvbghs.jpg",
     createdAt: "2026-05-21T11:41:07.741Z",
     id: "28603852-f918-4a37-a091-bf454bf39859",
-  },
-  {
-    title: "Steven's pancakes",
-    instructions: "Step 1: pancake\nStep 2: done :)",
-    ingredients: [
-      { name: "pancakes", measure: "1" },
-      { name: "joy", measure: "1" },
-    ],
-    imageUrl:
-      "https://res.cloudinary.com/dxef2u52q/image/upload/v1779363667/pltusiwhatyvnjrvbghs.jpg",
-    createdAt: "2026-05-21T11:41:07.741Z",
-    id: "18603852-f918-4a37-a091-bf454bf39859",
   },
 ];
