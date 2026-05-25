3a2da7b85f0c5199dbb8e92efac83103db70d191
Carolina <carolina.warntorp@outlook.com>
2026-05-25 09:53:59 +0200

    ref redesign list

--- src/components/RecipeCardList.jsx
+++ src/components/RecipeCardList.jsx
@@ -6,17 +6,20 @@ export const RecipeCardList = ({ arr }) => {
   const page = useLocation().pathname;
   return (
     <div
-      className={`mt-4 mx-auto
-                flex flex-wrap gap-3 items-stretch justify-center w-full
-      ${page !== "/" && `max-w-[1250px]`}`}
+      className={`mt-4 mx-auto justify-center gap-3 ${
+        page === "/"
+          ? `flex flex-wrap items-stretchw-full}`
+          : `grid max-w-[1250px]
+            grid-cols-[repeat(auto-fit,260px)]`
+      } `}
     >
       {arr.map((r) => {
         const id = r.id ? r.id : r.idMeal;
         return (
           <div
             key={id}
-            className={`flex basis-[250px] grow 
-            ${page === "/" ? `max-w-[30%]` : `max-w-[300px] lg:max-w-[400px]`}`}
+            className={`flex 
+            ${page === "/" ? `w-[30%]` : `w-[260px]`}`}
           >
             {page === "/" ? (
               <>
