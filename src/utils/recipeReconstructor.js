56cb0b73482136a222babb2d221642ee577b24b8
Carolina <carolina.warntorp@outlook.com>
2026-05-25 18:08:11 +0200

    ref breakout recipeToShow functionality

---
+++ src/utils/recipeReconstructor.js
@@ -0,0 +1,17 @@
+export const recipeReconstructor = (recipe) => {
+  const created = recipe.createdAt ? true : false;
+  const recipeObj = {
+    idMeal: created ? recipe.id : recipe.idMeal,
+    strMeal: created ? recipe.title : recipe.strMeal,
+    strMealThumb: created ? recipe.imageUrl : recipe.strMealThumb,
+    strCountry: created ? recipe.area : recipe.strCountry,
+    strTags: created
+      ? recipe.tags?.length
+        ? recipe.tags.toString()
+        : ""
+      : recipe.strTags,
+    strCategory: created ? recipe.category : recipe.strCategory,
+    rating: recipe.rating,
+  };
+  return recipeObj;
+};
