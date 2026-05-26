339ab594ddafbb9d80a0c512360ed3002389e633
Carolina <carolina.warntorp@outlook.com>
2026-05-26 19:51:49 +0200

    ref merge conflict fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,29 +4,18 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <header className="w-100vw bg-white/80">
-      <nav className="max-w-[1500px] mx-auto flex items-center justify-between p-2 pe-4 sm:px-8 sm:py-4">
+      <nav className="max-w-[1500px] mx-auto flex items-center justify-between">
         <NavLink to="/">
-        <div className="flex h-20 sm:h-10 max-w-[150px] mt-1 ms-1">
-          <img
-            src="/logos/new-logo.png"
-            className="h-full object-contain"
-            alt="Receptboken logo"
-          />
-        </div>
-      </NavLink>
-      {/* the "isActive" changes to color of the button depending on what page you are on */}
-      <div className="flex flex-col sm:flex-row sm:gap-8 p-2 pe-4 sm:px-4 sm:py-2 ">
-        <NavLink
-          to="/search"
-          className={({ isActive }) =>
-            `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
-          }
-        >
-          Search
-
+          <div className="flex h-20 sm:h-10 max-w-[150px] mt-1 ms-1">
+            <img
+              src="/logos/new-logo.png"
+              className="h-full object-contain"
+              alt="Receptboken logo"
+            />
+          </div>
         </NavLink>
         {/* the "isActive" changes to color of the button depending on what page you are on */}
-        <div className="flex flex-col sm:flex-row sm:gap-8">
+        <div className="flex flex-col sm:flex-row sm:gap-8 p-2 pe-4 sm:px-4 sm:py-2 ">
           <NavLink
             to="/search"
             className={({ isActive }) =>
@@ -35,23 +24,34 @@ function Navbar() {
           >
             Search
           </NavLink>
+          {/* the "isActive" changes to color of the button depending on what page you are on */}
+          <div className="flex flex-col sm:flex-row sm:gap-8">
+            <NavLink
+              to="/search"
+              className={({ isActive }) =>
+                `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
+              }
+            >
+              Search
+            </NavLink>
 
-          <NavLink
-            to="/my-recipes"
-            className={({ isActive }) =>
-              `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
-            }
-          >
-            My Recipes
-          </NavLink>
-          <NavLink
-            to="/create"
-            className={({ isActive }) =>
-              `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
-            }
-          >
-            Create Recipe
-          </NavLink>
+            <NavLink
+              to="/my-recipes"
+              className={({ isActive }) =>
+                `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
+              }
+            >
+              My Recipes
+            </NavLink>
+            <NavLink
+              to="/create"
+              className={({ isActive }) =>
+                `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
+              }
+            >
+              Create Recipe
+            </NavLink>
+          </div>
         </div>
       </nav>
     </header>
