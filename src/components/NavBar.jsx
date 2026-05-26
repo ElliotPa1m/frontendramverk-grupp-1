cfc7fcf4c527771c4b7c37a39af8aae170005df4
Carolina <carolina.warntorp@outlook.com>
2026-05-26 18:57:51 +0200

    ref add header and max width

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,45 +3,47 @@ import { NavLink } from "react-router-dom";
 // Main navigation bar with links to all pages
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-2 pe-4 sm:px-8 sm:py-4 bg-white/80">
-      <NavLink to="/">
-        <div className="flex h-20 max-w-[150px]">
-          <img
-            src="/logos/new-logo.png"
-            className="h-full object-contain"
-            alt="Receptboken logo"
-          />
-        </div>
-      </NavLink>
-      {/* the "isActive" changes to color of the button depending on what page you are on */}
-      <div className="flex flex-col sm:flex-row sm:gap-8">
-        <NavLink
-          to="/search"
-          className={({ isActive }) =>
-            `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
-          }
-        >
-          Search
+    <header className="w-100vw bg-white/80">
+      <nav className="max-w-[1500px] mx-auto flex items-center justify-between p-2 pe-4 sm:px-8 sm:py-4">
+        <NavLink to="/">
+          <div className="flex h-20 max-w-[150px]">
+            <img
+              src="/logos/new-logo.png"
+              className="h-full object-contain"
+              alt="Receptboken logo"
+            />
+          </div>
         </NavLink>
+        {/* the "isActive" changes to color of the button depending on what page you are on */}
+        <div className="flex flex-col sm:flex-row sm:gap-8">
+          <NavLink
+            to="/search"
+            className={({ isActive }) =>
+              `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
+            }
+          >
+            Search
+          </NavLink>
 
-        <NavLink
-          to="/my-recipes"
-          className={({ isActive }) =>
-            `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
-          }
-        >
-          My Recipes
-        </NavLink>
-        <NavLink
-          to="/create"
-          className={({ isActive }) =>
-            `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
-          }
-        >
-          Create Recipe
-        </NavLink>
-      </div>
-    </nav>
+          <NavLink
+            to="/my-recipes"
+            className={({ isActive }) =>
+              `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
+            }
+          >
+            My Recipes
+          </NavLink>
+          <NavLink
+            to="/create"
+            className={({ isActive }) =>
+              `barlow-condensed-light hover:text-button transition-colors ${isActive ? "text-button" : "text-text"}`
+            }
+          >
+            Create Recipe
+          </NavLink>
+        </div>
+      </nav>
+    </header>
   );
 }
 
